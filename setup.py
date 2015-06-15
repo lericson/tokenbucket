84faadbe29c9d17e03f14529545ab144c7133820
Ludvig Ericson <ludvig@lericson.se>
2015-06-15 18:23:17 +0200

    Add manifest, README

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", encoding="utf-8") as r:
     readme_text = r.read()
 
 setup(name='tokenbucket',
-      version='1.1.0',
+      version='1.1.1',
       author='Ludvig Ericson',
       author_email='ludvig@lericson.se',
       long_description=readme_text,
